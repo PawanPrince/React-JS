921b2054070a71f927b75348cb2ee6fbf9544c97
Pawan Kumar <67819742+PawanPrince@users.noreply.github.com>
2025-05-12 18:59:49 +0530

    Add files via upload

---
+++ RJS.js
@@ -0,0 +1,22 @@
+OPEN---->Windows PowerShell
+
+
+ Get-ExecutionPolicy
+ 
+ 
+ 
+ if restricted then 
+ 
+ 
+ Set-ExecutionPolicy remoteSigned
+ 
+ then install Node
+ 
+ download node js (LTS version)
+  after installation
+  node -v 
+  npm -v 
+  npx -v 
+  ================================================================
+  
+  react folder -> cmd open --> command write - npx create-vite@latest ---> y and enter  -----project name --- myfirstapp---> select a fW----> React ---->  select variant  -- javaScript --->  cd myfirstapp ---  npm install ---npm run dev ---? link ---->copy open in chrome --->  vite+react count is zero
